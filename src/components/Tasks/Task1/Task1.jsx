dea0167848992cdb900dc3ccd171b2bcb5780cc3
martovich <mart-1984@mail.ru>
2020-03-12 01:33:51 +0300

    add header

--- src/components/Tasks/Task1/Task1.jsx
+++ src/components/Tasks/Task1/Task1.jsx
@@ -1,34 +1,24 @@
 import React from "react";
 import s from "./Task1.module.css"
 
+const Msg = (props) => (
+    <div className={props.msgPos}>
+        <div className={s.message}>
+            <p className={s.name}>{props.name}</p>
+            {props.msg}
+            <span className={s.timestamp}>{props.time}</span>
+        </div>
+    </div>
+
+);
+
 const Task1 = () => (
     <div className={s.task}>
-        <div className={s.message}>
-            <p className={s.name}>Артём Москалёв</p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet earum in incidunt mollitia, porro quam quas
-            totam. Alias consequuntur corporis cumque cupiditate debitis doloribus eaque esse est et eum explicabo
-            facilis fugit, hic in iste laborum, molestias necessitatibus non officiis porro quia quidem, ratione
-            reiciendis similique sit totam vel! Accusantium, commodi deserunt distinctio earum, error fuga ipsam maxime
-            minus, molestiae nisi porro praesentium quae qui quisquam rem repudiandae suscipit? Deleniti eius enim illum
-            itaque tempora! Accusamus consectetur consequuntur delectus doloremque dolorum earum eos fugit impedit iure
-            modi mollitia odio perspiciatis qui quod repudiandae saepe, sit? Adipisci assumenda beatae commodi
-            consectetur cumque delectus dolorem, doloribus dolorum enim, facere illo, iusto laboriosam minus nemo
-            nesciunt officiis perspiciatis possimus quam qui quos ratione repellendus reprehenderit sapiente soluta
-            ullam. Ab autem, cumque cupiditate dolore doloribus eligendi inventore magnam officiis praesentium totam!
-            Adipisci delectus ducimus illum iusto omnis perferendis porro provident quasi soluta! A animi aperiam
-            architecto, autem commodi culpa, cumque deserunt distinctio eaque eius eos exercitationem facilis fugiat
-            nemo nulla odit, optio qui quia sint temporibus unde velit veritatis. Animi distinctio dolorum illum
-            voluptates voluptatum? Ab autem, beatae blanditiis cumque doloremque ducimus ea error ex harum ipsa iusto
-            laborum magni mollitia nam neque obcaecati officiis pariatur perspiciatis possimus praesentium quas quo
-            rerum sapiente sequi ut vel veniam vero! A ad aliquam animi blanditiis cum debitis deserunt dolor eligendi
-            facilis fugiat id illum iusto laudantium modi neque nesciunt nobis odio officiis, perferendis praesentium
-            quo quos ratione reiciendis repellat sapiente tenetur velit. Commodi dolor dolorum excepturi nam quaerat,
-            quisquam unde. Ab distinctio fugiat laudantium modi porro quod ratione? Accusamus amet asperiores assumenda,
-            doloribus enim non nostrum officiis, quasi qui reprehenderit soluta veritatis voluptatibus. Dicta doloribus
-            eum molestiae nobis quaerat quas soluta unde voluptatibus. Adipisci aliquid consectetur cupiditate dolorem
-            enim id nostrum nulla similique soluta tempore. Magnam.
-            <span className={s.timestamp}>10:20</span>
-        </div>
+        <Msg name="Артём Москалёв" msg="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab consequuntur debitis eius illum iste labore
+            minus, neque nihil nostrum tenetur?" time="10:20" msgPos={s.usr1}/>
+        <Msg name="Дима К"
+             msg="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cumque id libero nam omnis provident ratione repellat saepe ullam voluptatem. Architecto commodi debitis facilis maiores possimus quia reprehenderit totam voluptate?"
+             time="10:22" msgPos={s.usr2}/>
     </div>
 );
 
