65f5786961e81f27d5aa98859d470029f43a0ac1
martovich <mart-1984@mail.ru>
2020-03-13 00:48:50 +0300

    add classNames css fail

--- src/components/Tasks/Task2/Task2.jsx
+++ src/components/Tasks/Task2/Task2.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import s from "./Task2.module.css"
+
+
 
 const qualitysArr = [
-    {quality: 'рационалист', style : ''},
-    {quality: 'комуннист', style:'colTask2'},
+    {quality: 'рационалист', style: ''},
+    {quality: 'комуннист', style: 'colTaskModule2'},
     {quality: 'анимешник', style: ''}
 ];
 
+
 class Task2 extends React.Component {
     render = () => {
-        let qArr = qualitysArr.map((arr, index) => <div key={index} className={arr.style}><span>{arr.quality}</span></div>);
+        let classNames = require('classnames');
+        let cx = classNames.bind(s);
+        // let qArr = qualitysArr.map((arr, index) => <div key={index} className={arr.style}><span>{arr.quality}</span></div>);
+        let qArr = qualitysArr.map((arr, index) => <div key={index} className={cx(`${arr.style}`)}><span>{arr.quality}</span></div>);
         return (
             <div>
                 {qArr}
@@ -16,4 +23,5 @@ class Task2 extends React.Component {
         );
     }
 }
+
 export default Task2;
