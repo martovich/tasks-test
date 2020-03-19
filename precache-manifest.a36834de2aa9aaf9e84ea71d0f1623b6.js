9aa88136321740031facd826565f72483d565593
martovich <mart-1984@mail.ru>
2020-03-19 17:35:56 +0300

    Updates

---
+++ precache-manifest.a36834de2aa9aaf9e84ea71d0f1623b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "925e51f5c453c1718c710478c3424d37",
+    "url": "/tasks-test/index.html"
+  },
+  {
+    "revision": "60f0773b7ad2591d258b",
+    "url": "/tasks-test/static/css/main.33c7dce4.chunk.css"
+  },
+  {
+    "revision": "914d987ccb953686e43c",
+    "url": "/tasks-test/static/js/2.133288bb.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/tasks-test/static/js/2.133288bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f0773b7ad2591d258b",
+    "url": "/tasks-test/static/js/main.4d343414.chunk.js"
+  },
+  {
+    "revision": "504ca3dd046d797e5d35",
+    "url": "/tasks-test/static/js/runtime-main.06a2ca7d.js"
+  }
+]);
